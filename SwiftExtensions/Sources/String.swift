559ba9d143e98ea04779183da96888be44df904c
lw0717 <abc414181886@126.com>
2017-03-31 14:08:53 +0800

    为string添加length属性

---
+++ SwiftExtensions/Sources/String.swift
@@ -0,0 +1,17 @@
+//
+//  String.swift
+//  SwiftExtensions
+//
+//  Created by lw0717 on 17/3/31.
+//  Copyright © 2017年 lw0717. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+    /// String length
+    var length: Int {
+        return self.characters.count
+    }
+}
