e9e2eedf1c6047ff8d2e3ba5c13f0f4bf584dc44
lw0717 <abc414181886@126.com>
2017-04-07 11:34:44 +0800

    Add an extension for string to implement MD5

---
+++ SwiftExtensions/Sources/Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  SwiftExtensions
+//
+//  Created by lw0717 on 17/4/7.
+//  Copyright © 2017年 lw0717. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import <CommonCrypto/CommonDigest.h>
+
+#endif /* Bridging_Header_h */
--- SwiftExtensions/Sources/String.swift
+++ SwiftExtensions/Sources/String.swift
@@ -25,3 +25,23 @@ extension String {
         return self.removingPercentEncoding
     }
 }
+
+extension String {
+    /// MD5
+    var MD5: String{
+        let str = self.cString(using: String.Encoding.utf8)
+        let strLength = CC_LONG(self.lengthOfBytes(using: String.Encoding.utf8))
+        let digestLength = Int(CC_MD5_DIGEST_LENGTH)
+        let result = UnsafeMutablePointer<CUnsignedChar>.allocate(capacity: digestLength)
+        
+        CC_MD5(str!, strLength, result)
+        
+        let md5 = NSMutableString()
+        for i in 0 ..< digestLength {
+            md5.appendFormat("%02x", result[i])
+        }
+        result.deinitialize()
+        
+        return String(format: md5 as String)
+    }
+}
