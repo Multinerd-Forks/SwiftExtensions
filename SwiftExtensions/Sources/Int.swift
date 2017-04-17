1c85a5af86e948f15e94e5d77682e6e12ae70d02
lw0717 <abc414181886@126.com>
2017-04-17 14:17:57 +0800

    Add an extension for Int  to returns abs(self)

---
+++ SwiftExtensions/Sources/Int.swift
@@ -0,0 +1,16 @@
+//
+//  Int.swift
+//  SwiftExtensions
+//
+//  Created by lw0717 on 17/4/17.
+//  Copyright © 2017年 lw0717. All rights reserved.
+//
+
+import Foundation
+
+extension Int {
+    /// returns abs(self)
+    var abs: Int {
+        return Swift.abs(self)
+    }
+}
