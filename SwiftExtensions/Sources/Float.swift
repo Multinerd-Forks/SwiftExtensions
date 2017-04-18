def84bf6712b976ad1e65144a59b693b3d84a80f
lw0717 <abc414181886@126.com>
2017-04-18 16:55:59 +0800

    Add an extension for Float to returns abs(self)

---
+++ SwiftExtensions/Sources/Float.swift
@@ -0,0 +1,16 @@
+//
+//  Float.swift
+//  SwiftExtensions
+//
+//  Created by lw0717 on 17/4/18.
+//  Copyright © 2017年 lw0717. All rights reserved.
+//
+
+import Foundation
+
+extension Float {
+    /// returns abs(self)
+    func abs () -> Float {
+        return fabsf(self)
+    }
+}
