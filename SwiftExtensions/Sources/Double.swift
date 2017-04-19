cf81efad626f5ffdf704453d0ce50fb393e21217
lw0717 <abc414181886@126.com>
2017-04-19 18:00:11 +0800

    Add an extension for Double to returns abs(self)

---
+++ SwiftExtensions/Sources/Double.swift
@@ -0,0 +1,16 @@
+//
+//  Double.swift
+//  SwiftExtensions
+//
+//  Created by lw0717 on 17/4/19.
+//  Copyright © 2017年 lw0717. All rights reserved.
+//
+
+import Foundation
+
+extension Double {
+    /// returns abs(self)
+    func abs () -> Double {
+        return Foundation.fabs(self)
+    }
+}
