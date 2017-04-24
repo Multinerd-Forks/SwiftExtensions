6d5c45d49655537c54fcaf811587f3cba4dac429
lw0717 <abc414181886@126.com>
2017-04-24 10:40:30 +0800

    Add  extensions for Int to check a number is even or odd

--- SwiftExtensions/Sources/Foundation/Int.swift
+++ SwiftExtensions/Sources/Foundation/Int.swift
@@ -13,4 +13,12 @@ extension Int {
     var abs: Int {
         return Swift.abs(self)
     }
+    /// Checks if a number is even. Return true if self is even
+    var isEven: Bool {
+        return (self % 2) == 0
+    }
+    /// Checks if a number is odd. Return true if self is odd
+    var isOdd: Bool {
+        return (self % 2) == 1
+    }
 }
