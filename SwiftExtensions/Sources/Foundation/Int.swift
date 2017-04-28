6ccf1546fa5873cf87b10b2f601a6301c7c9b9ab
lw0717 <abc414181886@126.com>
2017-04-28 10:40:47 +0800

    Add extensions for Int

--- SwiftExtensions/Sources/Foundation/Int.swift
+++ SwiftExtensions/Sources/Foundation/Int.swift
@@ -10,18 +10,24 @@ import Foundation
 
 extension Int {
     /// Absolute value
-    var abs: Int {
-        return Swift.abs(self)
-    }
+    var abs: Int { return Swift.abs(self) }
 }
 
 extension Int {
     /// Checks if a number is even. Return true if self is even
-    var isEven: Bool {
-        return (self % 2) == 0
-    }
+    var isEven: Bool { return (self % 2) == 0 }
     /// Checks if a number is odd. Return true if self is odd
-    var isOdd: Bool {
-        return (self % 2) == 1
-    }
+    var isOdd: Bool { return (self % 2) == 1 }
+}
+
+extension Int {
+    public var toUInt8: UInt8 { return UInt8(truncatingBitPattern: self) }
+    public var toInt8: Int8 { return Int8(truncatingBitPattern: self) }
+    public var toUInt16: UInt16 { return UInt16(truncatingBitPattern: self) }
+    public var toInt16: Int16 { return Int16(truncatingBitPattern: self) }
+    public var toUInt32: UInt32 { return UInt32(truncatingBitPattern: self) }
+    public var toInt32: Int32 { return Int32(truncatingBitPattern: self) }
+    //No difference if the platform is 32 or 64
+    public var toUInt64: UInt64 { return UInt64(self) }
+    public var toInt64: Int64 { return Int64(self) }
 }
